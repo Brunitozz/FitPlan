5973029ee96809ecefb2f43b17d783073943ede4
Brunitozz <122190257+Brunitozz@users.noreply.github.com>
2024-06-23 13:14:54 -0500

    "Updated google-services.json, android/build.gradle, and lib/main.dart files"

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:primera_aplicacion/firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'maquinas.dart';
 import 'Rutinas/rutinas.dart';
 import 'Rutinas/provider.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await initializeDateFormatting('es_ES', null);
   runApp(const MyApp());
 }
