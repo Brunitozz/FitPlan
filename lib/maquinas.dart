4304023c18e56cd0319032a9e3a4d0ee90d7aa0a
Brunitozz <122190257+Brunitozz@users.noreply.github.com>
2024-06-25 12:50:40 -0500

    a un paso para conectar con el esp32

--- lib/maquinas.dart
+++ lib/maquinas.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 class MaquinasScreen extends StatefulWidget {
   const MaquinasScreen({Key? key}) : super(key: key);
@@ -12,162 +10,58 @@ class MaquinasScreen extends StatefulWidget {
 class _MaquinasScreenState extends State<MaquinasScreen> {
   late Map<String, Map<String, dynamic>> _maquinasDeCorrer;
   late Map<String, Map<String, dynamic>> _bicicletasEstaticas;
+  static const String imagenMaquina_Ocupada = "assets/maquinas/maquina-ocupada.png";
+  static const String imagenMaquina_Libre = "assets/maquinas/maquina-libre.png";
+  static const String imagenBicicleta_Estatica_Ocupada = "assets/maquinas/bicicleta-estatica-ocupada.png";
+  static const String imagenBicicleta_Estatica_Libre = "assets/maquinas/bicicleta-estatica-libre.png";
 
   @override
   void initState() {
     super.initState();
+
     _maquinasDeCorrer = {
-      'maquina1': {
-        'imagen': 'assets/maquinas/maquina-libre.png',
-        'disponible': 0,
-      },
-      'maquina2': {
-        'imagen': 'assets/maquinas/maquina-ocupada.png',
-        'disponible': 1,
-      },
-      'maquina3': {
-        'imagen': 'assets/maquinas/maquina-ocupada.png',
-        'disponible': 1,
-      },
-      'maquina4': {
-        'imagen': 'assets/maquinas/maquina-ocupada.png',
-        'disponible': 1,
-      },
-      'maquina5': {
-        'imagen': 'assets/maquinas/maquina-libre.png',
-        'disponible': 0,
-      },
-      'maquina6': {
-        'imagen': 'assets/maquinas/maquina-libre.png',
-        'disponible': 0,
-      },
-      'maquina7': {
-        'imagen': 'assets/maquinas/maquina-ocupada.png',
-        'disponible': 1,
-      },
-      'maquina8': {
-        'imagen': 'assets/maquinas/maquina-ocupada.png',
-        'disponible': 1,
-      },
-      'maquina9': {
-        'imagen': 'assets/maquinas/maquina-ocupada.png',
-        'disponible': 1,
-      },
+      'maquina1': {'disponible': 0},
+      'maquina2': {'disponible': 1},
+      'maquina3': {'disponible': 1},
+      'maquina4': {'disponible': 1},
+      'maquina5': {'disponible': 0},
+      'maquina6': {'disponible': 0},
+      'maquina7': {'disponible': 1},
+      'maquina8': {'disponible': 1},
+      'maquina9': {'disponible': 1},
     };
 
     _bicicletasEstaticas = {
-      'bicicleta1': {
-        'imagen': 'assets/maquinas/bicicleta-estatica-libre.png',
-        'disponible': 0,
-      },
-      'bicicleta2': {
-        'imagen': 'assets/maquinas/bicicleta-estatica-ocupada.png',
-        'disponible': 1,
-      },
-      'bicicleta3': {
-        'imagen': 'assets/maquinas/bicicleta-estatica-ocupada.png',
-        'disponible': 1,
-      },
-      'bicicleta4': {
-        'imagen': 'assets/maquinas/bicicleta-estatica-ocupada.png',
-        'disponible': 1,
-      },
-      'bicicleta5': {
-        'imagen': 'assets/maquinas/bicicleta-estatica-libre.png',
-        'disponible': 0,
-      },
-      'bicicleta6': {
-        'imagen': 'assets/maquinas/bicicleta-estatica-libre.png',
-        'disponible': 0,
-      },
-      'bicicleta7': {
-        'imagen': 'assets/maquinas/bicicleta-estatica-ocupada.png',
-        'disponible': 1,
-      },
-      'bicicleta8': {
-        'imagen': 'assets/maquinas/bicicleta-estatica-ocupada.png',
-        'disponible': 1,
-      },
-      'bicicleta9': {
-        'imagen': 'assets/maquinas/bicicleta-estatica-ocupada.png',
-        'disponible': 1,
-      },
+      'bicicleta1': {'disponible': 0},
+      'bicicleta2': {'disponible': 1},
+      'bicicleta3': {'disponible': 1},
+      'bicicleta4': {'disponible': 1},
+      'bicicleta5': {'disponible': 0},
+      'bicicleta6': {'disponible': 0},
+      'bicicleta7': {'disponible': 1},
+      'bicicleta8': {'disponible': 1},
+      'bicicleta9': {'disponible': 1},
     };
   }
 
-  /*void getMaquinas() async {
-    CollectionReference collectionReference =
-        FirebaseFirestore.instance.collection("maquinasDeCorrer");
-    QuerySnapshot maquinasDeCorrer = await collectionReference.get();
-
-    if (maquinasDeCorrer.docs.length != 0) {
-      setState(() {
-        for (var maquina in maquinasDeCorrer.docs) {
-          var data = maquina.data() as Map<String, dynamic>;
-          _maquinasDeCorrer[maquina.id] = {
-            'imagen': data['imagen'],
-            'disponible': data['disponible'],
-          };
-        }
-      });
-    }
+  void _toggleAvailabilityMaquina(String key, Map<String, dynamic> item) async {
+    setState(() {
+      item['disponible'] = item['disponible'] == 0 ? 1 : 0;
+    });
   }
 
-  void getBicicletas() async {
-    CollectionReference collectionReference =
-        FirebaseFirestore.instance.collection("bicicletasEstaticas");
-    QuerySnapshot bicicletasEstaticas = await collectionReference.get();
-
-    if (bicicletasEstaticas.docs.length != 0) {
-      setState(() {
-        for (var maquina in bicicletasEstaticas.docs) {
-          var data = maquina.data() as Map<String, dynamic>;
-          _bicicletasEstaticas[maquina.id] = {
-            'imagen': data['imagen'],
-            'disponible': data['disponible'],
-          };
-        }
-      });
-    }
-  }*/
-
-  void _toggleAvailabilityMaquina(
-      String key, Map<String, dynamic> item) async {
+  void _toggleAvailabilityBicicleta(String key, Map<String, dynamic> item) async {
     setState(() {
-      if (item['disponible'] == 0) {
-        item['disponible'] = 1;
-        item['imagen'] = 'assets/maquinas/maquina-ocupada.png';
-      } else {
-        item['disponible'] = 0;
-        item['imagen'] = 'assets/maquinas/maquina-libre.png';
-      }
+      item['disponible'] = item['disponible'] == 0 ? 1 : 0;
     });
-
-    /*await FirebaseFirestore.instance.collection('maquinas').doc(key).set({
-      'disponible': item['disponible'],
-      'imagen': item['imagen'],
-    });*/
   }
 
-  void _toggleAvailabilityBicicleta(
-      String key, Map<String, dynamic> item) async {
-    setState(() {
-      if (item['disponible'] == 0) {
-        item['disponible'] = 1;
-        item['imagen'] = 'assets/maquinas/bicicleta-estatica-ocupada.png';
-      } else {
-        item['disponible'] = 0;
-        item['imagen'] = 'assets/maquinas/bicicleta-estatica-libre.png';
-      }
-    });
+  String _getImageForMaquina(bool isAvailable) {
+    return isAvailable ? imagenMaquina_Libre : imagenMaquina_Ocupada;
+  }
 
-    /*await FirebaseFirestore.instance
-        .collection('bicicletasEstaticas')
-        .doc(key)
-        .set({
-      'disponible': item['disponible'],
-      'imagen': item['imagen'],
-    });*/
+  String _getImageForBicicleta(bool isAvailable) {
+    return isAvailable ? imagenBicicleta_Estatica_Libre : imagenBicicleta_Estatica_Ocupada;
   }
 
   @override
@@ -184,12 +78,14 @@ class _MaquinasScreenState extends State<MaquinasScreen> {
               items: _maquinasDeCorrer,
               crossAxisCount: 3,
               onItemTap: _toggleAvailabilityMaquina,
+              getImage: _getImageForMaquina,
             ),
             SectionGrid(
               title: 'Bicicletas Estáticas',
               items: _bicicletasEstaticas,
               crossAxisCount: 3,
               onItemTap: _toggleAvailabilityBicicleta,
+              getImage: _getImageForBicicleta,
             ),
           ],
         ),
@@ -198,25 +94,20 @@ class _MaquinasScreenState extends State<MaquinasScreen> {
   }
 }
 
-class SectionGrid extends StatefulWidget {
+class SectionGrid extends StatelessWidget {
   final String title;
   final Map<String, Map<String, dynamic>> items;
   final int crossAxisCount;
   final void Function(String, Map<String, dynamic>) onItemTap;
+  final String Function(bool) getImage;
 
   const SectionGrid({
     required this.title,
     required this.items,
     required this.crossAxisCount,
     required this.onItemTap,
+    required this.getImage,
   });
-
-  @override
-  _SectionGridState createState() => _SectionGridState();
-}
-
-class _SectionGridState extends State<SectionGrid> {
-  final Map<int, bool> _hovering = {};
 
   @override
   Widget build(BuildContext context) {
@@ -225,7 +116,7 @@ class _SectionGridState extends State<SectionGrid> {
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(
-            widget.title,
+            title,
             style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
           ),
         ),
@@ -233,44 +124,37 @@ class _SectionGridState extends State<SectionGrid> {
           shrinkWrap: true,
           physics: NeverScrollableScrollPhysics(),
           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: widget.crossAxisCount, // Número de columnas
-            childAspectRatio: 1, // Aspecto de las celdas
+            crossAxisCount: crossAxisCount,
+            childAspectRatio: 1,
           ),
-          itemCount: widget.items.length, // Número de elementos (celdas)
+          itemCount: items.length,
           itemBuilder: (BuildContext context, int index) {
-            String key = widget.items.keys.elementAt(index);
-            Map<String, dynamic> item = widget.items[key]!;
-            Color cardColor = item['disponible'] == 0
-                ? Color(0xFFBBF246) // Color verde personalizado
-                : Color(0xFFFF3C3C); // Color rojo personalizado
+            String key = items.keys.elementAt(index);
+            Map<String, dynamic> item = items[key]!;
+            bool isAvailable = item['disponible'] == 0;
 
             return InkWell(
               onTap: () {
-                widget.onItemTap(key, item);
+                onItemTap(key, item);
               },
-              child: AnimatedContainer(
-                duration: Duration(milliseconds: 200),
-                margin: EdgeInsets.symmetric(
-                    vertical: _hovering[index] ?? false ? 0 : 10),
-                child: Card(
-                  color: cardColor,
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: Image.asset(
-                          item['imagen'],
-                          fit: BoxFit.cover,
-                        ),
+              child: Card(
+                color: isAvailable ? Color(0xFFBBF246) : Color(0xFFFF3C3C),
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Image.asset(
+                        getImage(isAvailable),
+                        fit: BoxFit.cover,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          '${widget.title.split(' ')[0]} ${index + 1}', // Nombre del ítem
-                          style: TextStyle(fontSize: 16),
-                        ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        '${title.split(' ')[0]} ${index + 1}',
+                        style: TextStyle(fontSize: 16),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             );
